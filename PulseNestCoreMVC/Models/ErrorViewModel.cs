85f3afb6a45bd1c8074b2340e944c4b530588c5c
Luis Agudo Segrelles <lagudose@everis.com>
2019-04-27 20:49:35 +0200

    Agregar archivos de proyecto.

---
+++ PulseNestCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PulseNestCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
